374efdbe2f2670a8403765a28c5476e54d7cf162
marchabib <marc.habib@devinci.fr>
2017-02-22 00:52:12 +0100

    First preview of index

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <div class="main_container">
+
+
+            <div class="prof">
+                Professeur1
+            </div>
+
+            <div class="prof">
+                <h4>M. HABIB</h4>
+                <p>Occupé : <?php require 'marc.php' ?></p>
+            </div>
+
+            <div class="prof">
+                Professeur3
+            </div>
+
+    </div>
+
+  </body>
+</html>
